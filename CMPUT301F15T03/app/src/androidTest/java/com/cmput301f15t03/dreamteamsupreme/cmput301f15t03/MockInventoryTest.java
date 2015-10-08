e1ea957d6462a96a19980efd7e2180d8941359a0
Kyle O'Shaughnessy <kyle.oshaughnessy@gmail.com>
2015-10-08 17:38:37 -0600

    Added mock inventory test skeleton

---
+++ CMPUT301F15T03/app/src/androidTest/java/com/cmput301f15t03/dreamteamsupreme/cmput301f15t03/MockInventoryTest.java
@@ -0,0 +1,9 @@
+package com.cmput301f15t03.dreamteamsupreme.cmput301f15t03;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+public class MockInventoryTest extends ActivityInstrumentationTestCase2{
+    public MockInventoryTest(Class activityClass) {
+        super(activityClass);
+    }
+}
