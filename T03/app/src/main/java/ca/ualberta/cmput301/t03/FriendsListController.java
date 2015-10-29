dfc823144db6d971e30fd24fe04e15037fc8aa5b
Ross Anderson <ross.anderson@ualberta.ca>
2015-10-29 17:32:09 -0600

    Friends: add stubs for views & controllers

---
+++ T03/app/src/main/java/ca/ualberta/cmput301/t03/FriendsListController.java
@@ -0,0 +1,16 @@
+package ca.ualberta.cmput301.t03;
+
+/**
+ * Created by ross on 15-10-29.
+ */
+public class FriendsListController {
+    private FriendsList friendsList;
+
+    public void addFriend(User friend) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void removeFriend(User friend) {
+        throw new UnsupportedOperationException();
+    }
+}
