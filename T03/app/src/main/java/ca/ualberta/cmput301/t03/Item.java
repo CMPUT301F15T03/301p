5a6ff440a1481cc511c448a5795b13214a1d24c4
mabuyo <mabuyo@ualberta.ca>
2015-10-29 16:59:53 -0600

    Pulling from master.

--- T03/app/src/main/java/ca/ualberta/cmput301/t03/Item.java
+++ T03/app/src/main/java/ca/ualberta/cmput301/t03/Item.java
@@ -1,5 +1,7 @@
 package ca.ualberta.cmput301.t03;
 
+import java.util.Collection;
+
 /**
  * Created by mmabuyo on 2015-10-29.
  */
@@ -14,4 +16,51 @@ public class Item {
     private boolean itemIsPrivate;
     private String itemDescription;
 
+    public String getItemName() {
+        return itemName;
+    }
+
+    public void setItemName(String itemName) {
+        this.itemName = itemName;
+    }
+
+    public int getItemQuantity() {
+        return itemQuantity;
+    }
+
+    public void setItemQuantity(int itemQuantity) {
+        this.itemQuantity = itemQuantity;
+    }
+
+    public String getItemQuality() {
+        return itemQuality;
+    }
+
+    public void setItemQuality(String itemQuality) {
+        this.itemQuality = itemQuality;
+    }
+
+    public String getItemCategory() {
+        return itemCategory;
+    }
+
+    public void setItemCategory(String itemCategory) {
+        this.itemCategory = itemCategory;
+    }
+
+    public boolean isItemIsPrivate() {
+        return itemIsPrivate;
+    }
+
+    public void setItemIsPrivate(boolean itemIsPrivate) {
+        this.itemIsPrivate = itemIsPrivate;
+    }
+
+    public String getItemDescription() {
+        return itemDescription;
+    }
+
+    public void setItemDescription(String itemDescription) {
+        this.itemDescription = itemDescription;
+    }
 }
