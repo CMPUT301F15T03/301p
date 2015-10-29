d70b97e4626112cd1e98007e2eef18bb499a95af
Ross Anderson <ross.anderson@ualberta.ca>
2015-10-29 17:32:09 -0600

    Friends: add stubs for views & controllers

---
+++ T03/app/src/main/java/ca/ualberta/cmput301/t03/FriendsSearchController.java
@@ -0,0 +1,16 @@
+package ca.ualberta.cmput301.t03;
+
+/**
+ * Created by ross on 15-10-29.
+ */
+public class FriendsSearchController {
+    private FriendsList friendsList;
+
+    public void setQuery(String query) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void clearQuery() {
+        throw new UnsupportedOperationException();
+    }
+}
---
+++ T03/app/src/main/java/ca/ualberta/cmput301/t03/UserProfileController.java
@@ -0,0 +1,24 @@
+package ca.ualberta.cmput301.t03;
+
+/**
+ * Created by ross on 15-10-29.
+ */
+public class UserProfileController {
+    private UserProfile userProfile;
+
+    public void setName(String name) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void setPhone(String phone) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void setCity(String city) {
+        throw new UnsupportedOperationException();
+    }
+
+    public void setEmail(String email) {
+        throw new UnsupportedOperationException();
+    }
+}
