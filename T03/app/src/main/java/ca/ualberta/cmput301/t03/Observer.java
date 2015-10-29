530c00c73ff5e7c96c47c3e86e1f136c5623d496
Ross Anderson <ross.anderson@ualberta.ca>
2015-10-29 16:38:56 -0600

    Observer is no longer generic, just a naked Observer

--- T03/app/src/main/java/ca/ualberta/cmput301/t03/Observer.java
+++ T03/app/src/main/java/ca/ualberta/cmput301/t03/Observer.java
@@ -3,6 +3,6 @@ package ca.ualberta.cmput301.t03;
 /**
  * Created by ross on 15-10-29.
  */
-public interface Observer<T extends Observable> {
-    void update(T observable);
+public interface Observer {
+    void update(Observable observable);
 }
