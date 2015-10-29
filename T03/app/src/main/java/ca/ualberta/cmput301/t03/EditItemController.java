b313aaf83382b4cb04b02bb825ab0ceddd68010f
mabuyo <mabuyo@ualberta.ca>
2015-10-29 16:25:39 -0600

    Added Items and Photos stubs.

---
+++ T03/app/src/main/java/ca/ualberta/cmput301/t03/EditItemController.java
@@ -0,0 +1,10 @@
+package ca.ualberta.cmput301.t03;
+
+/**
+ * Created by mmabuyo on 2015-10-29.
+ */
+public class EditItemController {
+    private Item itemModel;
+
+    public void saveItemToInvetory() { throw new UnsupportedOperationException(); }
+}
