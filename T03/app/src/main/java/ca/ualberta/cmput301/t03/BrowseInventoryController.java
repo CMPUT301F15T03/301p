635f30f473389a44fe129b360ed75adca7d76a6c
Ross Anderson <ross.anderson@ualberta.ca>
2015-10-29 18:09:24 -0600

    Inventory: add stubs for views & controllers

---
+++ T03/app/src/main/java/ca/ualberta/cmput301/t03/BrowseInventoryController.java
@@ -0,0 +1,12 @@
+package ca.ualberta.cmput301.t03;
+
+/**
+ * Created by ross on 15-10-29.
+ */
+public class BrowseInventoryController {
+    private BrowsableInventories browsableInventories;
+
+    public void addFriendFilter(Filter filter) {
+        throw new UnsupportedOperationException();
+    }
+}
