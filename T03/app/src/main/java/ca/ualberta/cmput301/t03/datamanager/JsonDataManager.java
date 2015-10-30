fb7f1b0e20f2c27c34d2b8e65c91d86a6ab94465
Udey Rishi <udey@ualberta.ca>
2015-10-30 16:23:33 -0600

    Added the ability to provide explicit use of @Expose annotation in JsonDataManager through JsonFormatter

--- T03/app/src/main/java/ca/ualberta/cmput301/t03/datamanager/JsonDataManager.java
+++ T03/app/src/main/java/ca/ualberta/cmput301/t03/datamanager/JsonDataManager.java
@@ -10,18 +10,54 @@ import java.lang.reflect.Type;
  */
 public abstract class JsonDataManager implements DataManager {
 
-    private final Gson gson;
-
-    public JsonDataManager() {
-        // Source: http://www.mkyong.com/java/how-to-enable-pretty-print-json-output-gson/
-        gson = new GsonBuilder().setPrettyPrinting().create();
-    }
+    private static final boolean USE_PRETTY_JSON = true;
 
     protected String serialize(Object obj, Type type) {
+        return serialize(obj, type, null);
+    }
+
+    protected String serialize(Object obj, Type type, JsonFormatter format) {
+        Gson gson = getGsonFromJsonFormatter(format);
         return gson.toJson(obj, type);
     }
 
     protected <T> T deserialize(String obj, Type typeOfT) {
+        return deserialize(obj, typeOfT, null);
+    }
+
+    protected <T> T deserialize(String obj, Type typeOfT, JsonFormatter format) {
+        Gson gson = getGsonFromJsonFormatter(format);
         return gson.fromJson(obj, typeOfT);
+    }
+
+    private Gson getGsonFromJsonFormatter(JsonFormatter format) {
+        if (format == null) {
+            format = new JsonFormatter(false);
+        }
+
+        GsonBuilder gsonBuilder = new GsonBuilder();
+
+        if (USE_PRETTY_JSON) {
+            gsonBuilder.setPrettyPrinting();
+        }
+
+        if (format.useExplicitExposeAnnotation) {
+            gsonBuilder.excludeFieldsWithoutExposeAnnotation();
+        }
+
+        return gsonBuilder.create();
+    }
+
+    public static class JsonFormatter {
+
+        private boolean useExplicitExposeAnnotation;
+
+        public JsonFormatter(boolean useExplicitExposeAnnotation) {
+            setUseExplicitExposeAnnotation(useExplicitExposeAnnotation);
+        }
+
+        public void setUseExplicitExposeAnnotation(boolean useExplicitExposeAnnotation) {
+            this.useExplicitExposeAnnotation = useExplicitExposeAnnotation;
+        }
     }
 }
