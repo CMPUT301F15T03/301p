1d826e039da2578105d0c0cf37ebda08503b1ced
Kyle O'Shaughnessy <kyle.oshaughnessy@gmail.com>
2015-10-30 08:33:07 -0600

    Configuration: Added GPL3 header to source code.

--- T03/app/src/main/java/ca/ualberta/cmput301/t03/configuration/Configuration.java
+++ T03/app/src/main/java/ca/ualberta/cmput301/t03/configuration/Configuration.java
@@ -1,3 +1,25 @@
+/*
+    Copyright (C) 2015 "CMPUT301F15T03", Kyle O'Shaughnessy
+    Photography equipment trading application for CMPUT 301 at the University of Alberta
+
+
+    This file is part of {Application Name}.
+
+
+    {Application Name} is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package ca.ualberta.cmput301.t03.configuration;
 
 import android.content.Context;
@@ -5,29 +27,20 @@ import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
 
-import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.Set;
-import java.util.concurrent.CopyOnWriteArrayList;
 
 import ca.ualberta.cmput301.t03.Observable;
 import ca.ualberta.cmput301.t03.Observer;
-import ca.ualberta.cmput301.t03.datamanager.DataManager;
 
-/**
- * Created by kyleoshaughnessy on 2015-10-29.
- */
-public class Configuration implements Observable{
+public class Configuration implements Observable {
 
+    private transient static final String key = "CONFIGURATION";
     private transient Context context;
     private transient Set<Observer> observers;
     private Boolean offlineModeEnabled;
     private Boolean downloadImagesEnabled;
-
-    private transient static final String key = "CONFIGURATION";
 
     public Configuration(Context context) {
         this.observers = new HashSet<>();
--- T03/app/src/main/java/ca/ualberta/cmput301/t03/configuration/ConfigurationActivity.java
+++ T03/app/src/main/java/ca/ualberta/cmput301/t03/configuration/ConfigurationActivity.java
@@ -1,19 +1,36 @@
+/*
+    Copyright (C) 2015 "CMPUT301F15T03", Kyle O'Shaughnessy
+    Photography equipment trading application for CMPUT 301 at the University of Alberta
+
+
+    This file is part of {Application Name}.
+
+
+    {Application Name} is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package ca.ualberta.cmput301.t03.configuration;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 
 import ca.ualberta.cmput301.t03.Observable;
 import ca.ualberta.cmput301.t03.Observer;
 import ca.ualberta.cmput301.t03.R;
-import ca.ualberta.cmput301.t03.configuration.Configuration;
-import ca.ualberta.cmput301.t03.configuration.ConfigurationController;
 
 public class ConfigurationActivity extends AppCompatActivity implements Observer {
 
