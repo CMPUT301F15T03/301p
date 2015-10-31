b03766576819a23ce87b92f1b5c74ee9a01852b3
Udey Rishi <udey.rishi@gmail.com>
2015-10-30 23:20:54 -0600

    Refactored JsonFormatter -- moved methods to better locations

--- T03/app/src/main/java/ca/ualberta/cmput301/t03/datamanager/JsonDataManager.java
+++ T03/app/src/main/java/ca/ualberta/cmput301/t03/datamanager/JsonDataManager.java
@@ -1,7 +1,6 @@
 package ca.ualberta.cmput301.t03.datamanager;
 
 import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 
 import java.lang.reflect.Type;
 
@@ -10,41 +9,21 @@ import java.lang.reflect.Type;
  */
 public abstract class JsonDataManager implements DataManager {
 
-    private static final boolean USE_PRETTY_JSON = true;
-
     protected String serialize(Object obj, Type type) {
-        return serialize(obj, type, null);
+        return serialize(obj, type, new JsonFormatter(false, true));
     }
 
     protected String serialize(Object obj, Type type, JsonFormatter format) {
-        Gson gson = getGsonFromJsonFormatter(format);
+        Gson gson = format.getGson();
         return gson.toJson(obj, type);
     }
 
     protected <T> T deserialize(String obj, Type typeOfT) {
-        return deserialize(obj, typeOfT, null);
+        return deserialize(obj, typeOfT, new JsonFormatter(false, true));
     }
 
     protected <T> T deserialize(String obj, Type typeOfT, JsonFormatter format) {
-        Gson gson = getGsonFromJsonFormatter(format);
+        Gson gson = format.getGson();
         return gson.fromJson(obj, typeOfT);
-    }
-
-    private Gson getGsonFromJsonFormatter(JsonFormatter format) {
-        if (format == null) {
-            format = new JsonFormatter(false);
-        }
-
-        GsonBuilder gsonBuilder = new GsonBuilder();
-
-        if (USE_PRETTY_JSON) {
-            gsonBuilder.setPrettyPrinting();
-        }
-
-        if (format.useExplicitExposeAnnotation()) {
-            gsonBuilder.excludeFieldsWithoutExposeAnnotation();
-        }
-
-        return gsonBuilder.create();
     }
 }
--- T03/app/src/main/java/ca/ualberta/cmput301/t03/datamanager/JsonFormatter.java
+++ T03/app/src/main/java/ca/ualberta/cmput301/t03/datamanager/JsonFormatter.java
@@ -1,21 +1,47 @@
 package ca.ualberta.cmput301.t03.datamanager;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 /**
  * Created by rishi on 15-10-30.
  */
 public class JsonFormatter {
-
     private boolean useExplicitExposeAnnotation;
+    private boolean usePrettyJson;
 
-    public JsonFormatter(boolean useExplicitExposeAnnotation) {
+    public JsonFormatter(boolean useExplicitExposeAnnotation, boolean usePrettyJson) {
         setUseExplicitExposeAnnotation(useExplicitExposeAnnotation);
+        setUsePrettyJson(usePrettyJson);
     }
 
-    public boolean useExplicitExposeAnnotation() {
+    public boolean getUseExplicitExposeAnnotation() {
         return useExplicitExposeAnnotation;
     }
 
     public void setUseExplicitExposeAnnotation(boolean useExplicitExposeAnnotation) {
         this.useExplicitExposeAnnotation = useExplicitExposeAnnotation;
+    }
+
+    public boolean getUsePrettyJson() {
+        return usePrettyJson;
+    }
+
+    public void setUsePrettyJson(boolean usePrettyJson) {
+        this.usePrettyJson = usePrettyJson;
+    }
+
+    public Gson getGson() {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+
+        if (getUsePrettyJson()) {
+            gsonBuilder.setPrettyPrinting();
+        }
+
+        if (getUseExplicitExposeAnnotation()) {
+            gsonBuilder.excludeFieldsWithoutExposeAnnotation();
+        }
+
+        return gsonBuilder.create();
     }
 }
