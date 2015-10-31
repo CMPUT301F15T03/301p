f28c8559c7028596bcce7cd57920e5129c3fc884
Udey Rishi <udey@ualberta.ca>
2015-10-31 15:23:47 -0600

    Added HttpClient

--- T03/app/src/main/java/ca/ualberta/cmput301/t03/datamanager/httpdatamanager/HttpDataManager.java
+++ T03/app/src/main/java/ca/ualberta/cmput301/t03/datamanager/httpdatamanager/HttpDataManager.java
@@ -1,25 +1,23 @@
-package ca.ualberta.cmput301.t03.datamanager;
-
-import android.content.Context;
+package ca.ualberta.cmput301.t03.datamanager.httpdatamanager;
 
 import java.lang.reflect.Type;
+import java.net.MalformedURLException;
+import java.net.URL;
 
-import ca.ualberta.cmput301.t03.R;
 import ca.ualberta.cmput301.t03.common.Preconditions;
 import ca.ualberta.cmput301.t03.common.exceptions.NotImplementedException;
+import ca.ualberta.cmput301.t03.datamanager.DataKey;
+import ca.ualberta.cmput301.t03.datamanager.JsonDataManager;
 
 /**
  * Created by rishi on 15-10-30.
  */
 public class HttpDataManager extends JsonDataManager {
 
-    private final Context context;
-    private final String rootUti;
+    private final URL rootUrl;
 
-    public HttpDataManager(Context context) {
-        this.context = Preconditions.checkNotNull(context, "context");
-        this.rootUti = Preconditions.checkNotNullOrWhitespace(context.getString(R.string.httpDataManagerRootUri),
-                "httpDataManagerRootUri");
+    public HttpDataManager(String rootUrl) throws MalformedURLException {
+        this.rootUrl = new URL(Preconditions.checkNotNullOrWhitespace(rootUrl, "rootUrl"));
     }
 
     @Override
@@ -45,5 +43,13 @@ public class HttpDataManager extends JsonDataManager {
     @Override
     public boolean isOperational() {
         throw new NotImplementedException();
+    }
+
+    private URL getQueryUrl(DataKey key) {
+        try {
+            return new URL(new URL(rootUrl, key.getType()), key.getId());
+        } catch (MalformedURLException e) {
+            throw new IllegalArgumentException("The DataKey cannot be converted to a valid URL.", e);
+        }
     }
 }
