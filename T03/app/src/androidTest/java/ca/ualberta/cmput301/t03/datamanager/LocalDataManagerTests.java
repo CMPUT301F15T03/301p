175e8fbd1d49b6d945fa36c22f969725fac0c672
Udey Rishi <udey@ualberta.ca>
2015-10-31 00:07:29 -0600

    Extracted out DataManager API tests out into standard base class + interface

--- T03/app/src/androidTest/java/ca/ualberta/cmput301/t03/datamanager/LocalDataManagerTests.java
+++ T03/app/src/androidTest/java/ca/ualberta/cmput301/t03/datamanager/LocalDataManagerTests.java
@@ -1,63 +1,37 @@
 package ca.ualberta.cmput301.t03.datamanager;
 
-import android.test.AndroidTestCase;
-
-import com.google.gson.reflect.TypeToken;
-
-import static ca.ualberta.cmput301.t03.commontesting.ExceptionAsserter.assertThrowsException;
-
 /**
  * Created by rishi on 15-10-30.
  */
-// Source: http://developer.android.com/reference/android/test/AndroidTestCase.html
-public class LocalDataManagerTests extends AndroidTestCase {
+public class LocalDataManagerTests extends BaseDataManagerTests<LocalDataManager> implements DataManagerApiTests {
 
-    private LocalDataManager localDataManager;
-    private TestDto testDto;
-    private DataKey dataKey;
-
-    public void setUp() {
-        localDataManager = new LocalDataManager(this.getContext());
-        testDto = new TestDto(100, "Hundred", false, "a hidden string");
-        dataKey = new DataKey("TestDto", "123");
+    @Override
+    protected LocalDataManager createNewDataManager() {
+        return new LocalDataManager(getContext());
     }
 
+    @Override
     public void testKeyExists() {
-        assertFalse(localDataManager.keyExists(dataKey));
-        localDataManager.writeData(dataKey, testDto, new TypeToken<TestDto>() {}.getType());
-        assertTrue(localDataManager.keyExists(dataKey));
-        assertFalse(localDataManager.keyExists(new DataKey("not", "exists")));
-        assertTrue(localDataManager.deleteIfExists(dataKey));
+        super.keyExistsTest();
     }
 
+    @Override
     public void testGetDataWhenKeyDoesNotExistThrowsException() {
-        assertFalse(localDataManager.keyExists(dataKey));
-        assertThrowsException(new Runnable() {
-            @Override
-            public void run() {
-                localDataManager.getData(dataKey, new TypeToken<TestDto>() {}.getType());
-            }
-        }, DataKeyNotFoundException.class);
+        super.getDataWhenKeyDoesNotExistThrowsExceptionTest();
     }
 
+    @Override
     public void testWriteData() {
-        localDataManager.writeData(dataKey, testDto, new TypeToken<TestDto>() {}.getType());
-        assertTrue(localDataManager.keyExists(dataKey));
-        TestDto receivedData = localDataManager.getData(dataKey, new TypeToken<TestDto>() {}.getType());
-        assertEquals(testDto, receivedData);
-        assertTrue(localDataManager.deleteIfExists(dataKey));
+        super.writeDataTest();
     }
 
+    @Override
     public void testDelete() {
-        assertFalse(localDataManager.keyExists(dataKey));
-        assertFalse(localDataManager.deleteIfExists(dataKey));
-        localDataManager.writeData(dataKey, testDto, new TypeToken<TestDto>() {}.getType());
-        assertTrue(localDataManager.keyExists(dataKey));
-        assertTrue(localDataManager.deleteIfExists(dataKey));
-        assertFalse(localDataManager.keyExists(dataKey));
+        super.deleteTest();
     }
 
+    @Override
     public void testIsOperational() {
-        assertTrue(localDataManager.isOperational());
+        super.isOperationalTest();
     }
 }
