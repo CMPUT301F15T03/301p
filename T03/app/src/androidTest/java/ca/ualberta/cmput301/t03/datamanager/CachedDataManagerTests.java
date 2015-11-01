76db6d3dc9afb369e5ad6c7d9b0e358bb2df4e95
Udey Rishi <udey@ualberta.ca>
2015-11-01 00:40:03 -0600

    Implemented CachedDataManager

---
+++ T03/app/src/androidTest/java/ca/ualberta/cmput301/t03/datamanager/CachedDataManagerTests.java
@@ -0,0 +1,130 @@
+package ca.ualberta.cmput301.t03.datamanager;
+
+import com.google.gson.reflect.TypeToken;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+import java.net.MalformedURLException;
+import java.util.HashMap;
+
+import ca.ualberta.cmput301.t03.common.exceptions.NotImplementedException;
+import ca.ualberta.cmput301.t03.common.exceptions.ServiceNotAvailableException;
+import ca.ualberta.cmput301.t03.datamanager.httpdatamanager.HttpDataManager;
+import static ca.ualberta.cmput301.t03.commontesting.ExceptionAsserter.assertThrowsException;
+
+/**
+ * Created by rishi on 15-10-31.
+ */
+public class CachedDataManagerTests extends BaseDataManagerTests<CachedDataManager> implements DataManagerApiTests {
+    @Override
+    protected CachedDataManager createNewDataManager() {
+        try {
+            return new CachedDataManager(new HttpDataManager(getContext()), getContext());
+        } catch (MalformedURLException e) {
+            throw new NotImplementedException("Invalid httpDataManagerRootUrl resource.", e);
+        }
+    }
+
+    @Override
+    public void testKeyExists() {
+        super.keyExistsTest();
+    }
+
+    @Override
+    public void testGetDataWhenKeyDoesNotExistThrowsException() {
+        super.getDataWhenKeyDoesNotExistThrowsExceptionTest();
+    }
+
+    @Override
+    public void testWriteData() {
+        super.writeDataTest();
+    }
+
+    @Override
+    public void testDelete() {
+        super.deleteTest();
+    }
+
+    @Override
+    public void testIsOperational() {
+        super.isOperationalTest();
+    }
+
+    public void testWriteThenGetDataWhenInnerDataManagerNotAvailable() throws IOException {
+
+        InMemoryDataManager mockDataManager = new InMemoryDataManager();
+        CachedDataManager cachedDataManager = new CachedDataManager(mockDataManager, getContext());
+
+        Type type = new TypeToken<TestDto>() {}.getType();
+
+        assertFalse(cachedDataManager.keyExists(dataKey));
+        cachedDataManager.writeData(dataKey, testDto, type);
+        mockDataManager.setIsOperational(false);
+        assertFalse(cachedDataManager.isOperational());
+        assertTrue(cachedDataManager.keyExists(dataKey));
+        TestDto retrieved = cachedDataManager.getData(dataKey, type);
+        assertEquals(testDto, retrieved);
+        mockDataManager.setIsOperational(true);
+        assertTrue(cachedDataManager.deleteIfExists(dataKey));
+        assertFalse(cachedDataManager.keyExists(dataKey));
+    }
+
+    public void testWriteWhenInnerManagerIsNotAvailableThrowsException() throws IOException {
+        InMemoryDataManager mockDataManager = new InMemoryDataManager();
+        final CachedDataManager cachedDataManager = new CachedDataManager(mockDataManager, getContext());
+
+        final Type type = new TypeToken<TestDto>() {}.getType();
+
+        assertFalse(cachedDataManager.keyExists(dataKey));
+        mockDataManager.setIsOperational(false);
+        assertFalse(cachedDataManager.isOperational());
+        assertThrowsException(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    cachedDataManager.writeData(dataKey, testDto, type); // throws
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }, ServiceNotAvailableException.class);
+        assertFalse(cachedDataManager.keyExists(dataKey));
+        mockDataManager.setIsOperational(true);
+        assertTrue(cachedDataManager.isOperational());
+        assertFalse(cachedDataManager.keyExists(dataKey));
+    }
+
+    private class InMemoryDataManager implements DataManager {
+        private boolean isOperational = true;
+        private HashMap<String, Object> inMemoryDataRepository = new HashMap<>();
+
+        @Override
+        public boolean keyExists(DataKey key) throws IOException {
+            return inMemoryDataRepository.containsKey(key.toString());
+        }
+
+        @Override
+        public <T> T getData(DataKey key, Type typeOfT) throws IOException {
+            return (T)inMemoryDataRepository.get(key.toString());
+        }
+
+        @Override
+        public <T> void writeData(DataKey key, T obj, Type typeOfT) throws IOException {
+            inMemoryDataRepository.put(key.toString(), obj);
+        }
+
+        @Override
+        public boolean deleteIfExists(DataKey key) throws IOException {
+            return inMemoryDataRepository.remove(key.toString()) != null;
+        }
+
+        @Override
+        public boolean isOperational() {
+            return isOperational;
+        }
+
+        public void setIsOperational(boolean isOperational) {
+            this.isOperational = isOperational;
+        }
+    }
+}
---
+++ T03/app/src/main/java/ca/ualberta/cmput301/t03/datamanager/CachedDataManager.java
@@ -0,0 +1,80 @@
+package ca.ualberta.cmput301.t03.datamanager;
+
+import android.content.Context;
+
+import java.io.IOException;
+import java.lang.reflect.Type;
+
+import ca.ualberta.cmput301.t03.common.Preconditions;
+import ca.ualberta.cmput301.t03.common.exceptions.ServiceNotAvailableException;
+
+/**
+ * Created by rishi on 15-10-31.
+ */
+public class CachedDataManager implements DataManager {
+
+    private final static String CACHE_DIRECTORY = "cache";
+    private final DataManager innerManager;
+    private final Context context;
+    private final LocalDataManager cachingDataManager;
+
+    public CachedDataManager(DataManager innerManager, Context context, boolean useExplicitExposeAnnotation) {
+        this.innerManager = Preconditions.checkNotNull(innerManager, "innerManager");
+        this.context = Preconditions.checkNotNull(context, "context");
+        this.cachingDataManager = new LocalDataManager(context, useExplicitExposeAnnotation);
+    }
+
+    public CachedDataManager(DataManager innerManager, Context context) {
+        this(innerManager, context, false);
+    }
+
+    @Override
+    public boolean keyExists(DataKey key) throws IOException {
+        if (innerManager.isOperational()) {
+            return innerManager.keyExists(key);
+        }
+
+        return cachingDataManager.keyExists(convertToCacheKey(key));
+    }
+
+    @Override
+    public <T> T getData(DataKey key, Type typeOfT) throws IOException {
+        if (innerManager.isOperational()) {
+            T retrievedData = innerManager.getData(key, typeOfT);
+            cachingDataManager.writeData(convertToCacheKey(key), retrievedData, typeOfT);
+            return retrievedData;
+        }
+
+        return cachingDataManager.getData(convertToCacheKey(key), typeOfT);
+    }
+
+    @Override
+    public <T> void writeData(DataKey key, T obj, Type typeOfT) throws IOException {
+        if (isOperational()) {
+            innerManager.writeData(key, obj, typeOfT);
+            cachingDataManager.writeData(convertToCacheKey(key), obj, typeOfT);
+        }
+        else {
+            throw new ServiceNotAvailableException("Inner DataManager is not operational. Cannot perform the write operation.");
+        }
+    }
+
+    @Override
+    public boolean deleteIfExists(DataKey key) throws IOException {
+        if (isOperational()) {
+            boolean existed = innerManager.deleteIfExists(key);
+            cachingDataManager.deleteIfExists(convertToCacheKey(key));
+            return existed;
+        }
+        throw new ServiceNotAvailableException("Inner DataManager is not operational. Cannot perform the delete operation.");
+    }
+
+    @Override
+    public boolean isOperational() {
+        return innerManager.isOperational();
+    }
+
+    private DataKey convertToCacheKey(DataKey key) {
+        return new DataKey(CACHE_DIRECTORY + "/" + key.getType(), key.getId());
+    }
+}
