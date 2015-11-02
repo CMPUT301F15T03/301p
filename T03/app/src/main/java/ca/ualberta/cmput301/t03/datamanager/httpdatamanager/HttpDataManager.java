34d6e36de1dec06ea3470bed849496f1b86fcaa8
John Slevinsky <john.slev@gmail.com>
2015-11-02 13:20:50 -0700

    Revert "Synchronous http client as default, this can't possibly cause any"
    This was pretty dumb idea.
    This reverts commit 7d77699d2a329f22fd69b8f5f18e2bc67b7004e7.

--- T03/app/src/main/java/ca/ualberta/cmput301/t03/datamanager/httpdatamanager/HttpDataManager.java
+++ T03/app/src/main/java/ca/ualberta/cmput301/t03/datamanager/httpdatamanager/HttpDataManager.java
@@ -3,7 +3,6 @@ package ca.ualberta.cmput301.t03.datamanager.httpdatamanager;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.provider.ContactsContract;
 
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
@@ -33,9 +32,6 @@ public class HttpDataManager extends JsonDataManager {
 
     private final HttpClient client;
     private final Context context;
-
-    private HttpResponse mResponse;
-    private DataKey mKey;
 
     public HttpDataManager(Context context, boolean useExplicitExposeAnnotation) throws MalformedURLException {
         super(useExplicitExposeAnnotation);
@@ -73,15 +69,11 @@ public class HttpDataManager extends JsonDataManager {
             throw new ServiceNotAvailableException("HttpDataManager is not operational. Cannot perform this operation.");
         }
 
-        mKey = key;
+
         Thread t = new Thread(new Runnable() {
             @Override
             public void run() {
-                try {
-                    mResponse = client.makeGetRequest(mKey.toString());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+                HttpResponse response = client.makeGetRequest(key.toString());
 
             }
         });
@@ -93,16 +85,16 @@ public class HttpDataManager extends JsonDataManager {
             e.printStackTrace();
         }
 
-        if (mResponse.getResponseCode() == HttpStatusCode.NOT_FOUND.getStatusCode()) {
+        if (response.getResponseCode() == HttpStatusCode.NOT_FOUND.getStatusCode()) {
             throw new DataKeyNotFoundException(key.toString());
         }
-        if (mResponse.getResponseCode() == HttpStatusCode.OK.getStatusCode()) {
-            String sourceJson = extractSourceFromElasticSearchHttpResponse(mResponse);
+        if (response.getResponseCode() == HttpStatusCode.OK.getStatusCode()) {
+            String sourceJson = extractSourceFromElasticSearchHttpResponse(response);
             return deserialize(sourceJson, typeOfT);
         }
 
         throw new NotImplementedException(String.format("Dev note: Unexpected response '%d' from the GET Elastic Search endpoint.",
-                mResponse.getResponseCode()));
+                response.getResponseCode()));
     }
 
     @Override
