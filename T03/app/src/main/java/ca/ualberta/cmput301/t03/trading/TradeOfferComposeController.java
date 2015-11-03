8ca642ab402121dc96bb203c86c45274f5d6b653
Ross Anderson <ross.anderson@ualberta.ca>
2015-11-03 14:18:05 -0700

    Trading: fragments->activities, implement controllers, move tradestate exception
    Sorry for long commit title everyone

--- T03/app/src/main/java/ca/ualberta/cmput301/t03/trading/TradeOfferComposeController.java
+++ T03/app/src/main/java/ca/ualberta/cmput301/t03/trading/TradeOfferComposeController.java
@@ -1,7 +1,43 @@
 package ca.ualberta.cmput301.t03.trading;
 
+import android.content.Context;
+import android.util.Log;
+
+import ca.ualberta.cmput301.t03.inventory.Item;
+import ca.ualberta.cmput301.t03.trading.exceptions.IllegalTradeStateTransition;
+
 /**
  * Created by ross on 15-10-29.
  */
 public class TradeOfferComposeController {
+    private final String logTAG = "TradeOfferCompose";
+    private Context context;
+
+    private Trade model;
+
+    public TradeOfferComposeController(Context context, Trade model) {
+        this.model = model;
+        this.context = context;
+    }
+
+    public void offerTrade() {
+        try {
+            model.offer();
+        } catch (IllegalTradeStateTransition illegalTradeStateTransition) {
+            Log.e(logTAG, illegalTradeStateTransition.getMessage());
+        }
+    }
+
+    public void cancelTrade() {
+        try {
+            model.cancel();
+        } catch (IllegalTradeStateTransition illegalTradeStateTransition) {
+            Log.e(logTAG, illegalTradeStateTransition.getMessage());
+        }
+    }
+
+    public void addBorrowerItem(Item item) {
+        model = new Trade(model.getBorrower(), model.getOwner(),
+                model.getBorrowersItems(), model.getOwnersItems(), this.context);
+    }
 }
--- T03/app/src/main/java/ca/ualberta/cmput301/t03/trading/TradeOfferReviewController.java
+++ T03/app/src/main/java/ca/ualberta/cmput301/t03/trading/TradeOfferReviewController.java
@@ -1,7 +1,50 @@
 package ca.ualberta.cmput301.t03.trading;
 
+import android.content.Context;
+import android.util.Log;
+
+import ca.ualberta.cmput301.t03.trading.exceptions.IllegalTradeStateTransition;
+
 /**
  * Created by ross on 15-10-29.
  */
 public class TradeOfferReviewController {
+    private final String logTAG = "TradeOfferReview";
+
+    private Context context;
+    private Trade model;
+
+    public TradeOfferReviewController(Context context, Trade model) {
+        this.context = context;
+        this.model = model;
+    }
+
+    public void acceptTrade() {
+        try {
+            model.accept();
+        } catch (IllegalTradeStateTransition illegalTradeStateTransition) {
+            Log.e(logTAG, illegalTradeStateTransition.getMessage());
+        }
+    }
+
+    public void declineTrade() {
+        try {
+            model.decline();
+        } catch (IllegalTradeStateTransition illegalTradeStateTransition) {
+            Log.e(logTAG, illegalTradeStateTransition.getMessage());
+        }
+    }
+
+    public void declineAndCounterTrade() {
+        try {
+            model.decline();
+        } catch (IllegalTradeStateTransition illegalTradeStateTransition) {
+            Log.e(logTAG, illegalTradeStateTransition.getMessage());
+        }
+        /**
+         * TODO perform required tasks for counter offer
+         * - create a new trade
+         * - send to compose trade view
+         */
+    }
 }
