f935dc12470ff112b077348d3348f70e9284d490
Quentin Lautischer <lautisch@ualberta.ca>
2015-11-03 10:36:52 -0700

    Added AddItemView Activity that InventoryController starts on FAB click

--- T03/app/src/main/java/ca/ualberta/cmput301/t03/user/UserInventoryController.java
+++ T03/app/src/main/java/ca/ualberta/cmput301/t03/user/UserInventoryController.java
@@ -1,14 +1,41 @@
 package ca.ualberta.cmput301.t03.user;
 
+import android.content.Context;
+import android.content.Intent;
+
+import java.net.MalformedURLException;
+
+import ca.ualberta.cmput301.t03.configuration.Configuration;
+import ca.ualberta.cmput301.t03.datamanager.DataManager;
+import ca.ualberta.cmput301.t03.datamanager.httpdatamanager.HttpDataManager;
+import ca.ualberta.cmput301.t03.inventory.AddItemView;
 import ca.ualberta.cmput301.t03.inventory.Inventory;
 
 /**
  * Created by ross on 15-10-29.
  */
 public class UserInventoryController {
-    private Inventory inventory;
+    Inventory inventory;
+    Context context;
+    Configuration configuration;
+    DataManager dataManager;
+
+    public UserInventoryController(Context context, Inventory inventory) {
+        this.inventory = inventory;
+        this.context = context;
+
+        configuration = new Configuration(context);
+        try {
+            dataManager = new HttpDataManager(context);
+        } catch (MalformedURLException e) {
+            throw new RuntimeException("There has been a issue contacting the application server.");
+        }
+    }
 
     public void addItemButtonClicked() {
-        throw new UnsupportedOperationException();
+//        throw new UnsupportedOperationException();
+        Intent intent = new Intent(context, AddItemView.class);
+//        intent.putExtra(EXTRA_MESSAGE, message);
+        context.startActivity(intent);
     }
 }
