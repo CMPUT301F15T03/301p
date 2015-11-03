210077c6bf6176760365885853f7140606ee78e9
Quentin Lautischer <lautisch@ualberta.ca>
2015-11-02 23:52:57 -0700

    Added onClickListeners for Broswe and Inventory, not connected to controllers yet

--- T03/app/src/main/java/ca/ualberta/cmput301/t03/inventory/ItemTileFragment.java
+++ T03/app/src/main/java/ca/ualberta/cmput301/t03/inventory/ItemTileFragment.java
@@ -4,10 +4,15 @@ import android.app.Activity;
 import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
@@ -16,7 +21,7 @@ import ca.ualberta.cmput301.t03.Observer;
 import ca.ualberta.cmput301.t03.R;
 
 
-public class ItemTileFragment extends Fragment implements Observer {
+public class ItemTileFragment extends Fragment implements Observer{
     private Item model;
 
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -45,13 +50,17 @@ public class ItemTileFragment extends Fragment implements Observer {
             mName = getArguments().getString(ARG_NAME);
             mCategory = getArguments().getString(ARG_CATEGORY);
         }
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_item_tile, container, false);
+        View v = inflater.inflate(R.layout.fragment_item_tile, container, false);
+
+        return v;
     }
 
 
@@ -59,7 +68,5 @@ public class ItemTileFragment extends Fragment implements Observer {
     public void update(Observable observable) {
         throw new UnsupportedOperationException();
     }
-
-
 
 }
