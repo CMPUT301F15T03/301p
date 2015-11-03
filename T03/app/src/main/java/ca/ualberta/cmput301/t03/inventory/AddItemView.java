cce4ceb653c9f504c6a9ef9f5a3959d57f5c214d
mabuyo <mabuyo@ualberta.ca>
2015-11-03 13:35:41 -0700

    Can now add items to user inventory, however inventory view needs a refresh before new item shows up. Need to fix that.

--- T03/app/src/main/java/ca/ualberta/cmput301/t03/inventory/AddItemView.java
+++ T03/app/src/main/java/ca/ualberta/cmput301/t03/inventory/AddItemView.java
@@ -6,23 +6,57 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.net.MalformedURLException;
+
 import ca.ualberta.cmput301.t03.R;
+import ca.ualberta.cmput301.t03.configuration.Configuration;
+import ca.ualberta.cmput301.t03.user.User;
+import ca.ualberta.cmput301.t03.user.UserInventoryController;
 
 public class AddItemView extends AppCompatActivity {
     private Item itemModel;
     private AddItemController controller;
     private Button addToInventoryButton;
+    private Inventory inventoryModel;
+
+    private FloatingActionButton fab;
+    private User user;
+    private Activity activity = this;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_add_item_view);
 
-        controller = new AddItemController(this, findViewById(android.R.id.content));
+        Configuration c = new Configuration(this.getBaseContext());
+        c.getApplicationUserName();
+
+        user = null;
+        try {
+            user = new User(c.getApplicationUserName(), this.getBaseContext());
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        Thread worker = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    inventoryModel = user.getInventory();
+                    controller = new AddItemController(findViewById(R.id.add_item_view), activity, inventoryModel);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+        worker.start();
 
         addToInventoryButton = (Button) findViewById(R.id.addItem);
 
