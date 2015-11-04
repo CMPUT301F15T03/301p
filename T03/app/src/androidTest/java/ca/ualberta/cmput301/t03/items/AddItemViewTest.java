49bf51ce4c7ec54bfc70c2d3738a960668289004
mabuyo <mabuyo@ualberta.ca>
2015-11-03 21:00:03 -0700

    Using the new PrimaryUser.getInstance for adding, editing and inspecting items.

---
+++ T03/app/src/androidTest/java/ca/ualberta/cmput301/t03/items/AddItemViewTest.java
@@ -0,0 +1,25 @@
+package ca.ualberta.cmput301.t03.items;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import ca.ualberta.cmput301.t03.inventory.AddItemView;
+
+/**
+ * Created by mmabuyo on 2015-11-03.
+ */
+public class AddItemViewTest extends ActivityInstrumentationTestCase2 {
+
+    public AddItemViewTest() {
+        super(ca.ualberta.cmput301.t03.inventory.AddItemView.class);
+    }
+
+    public void AddItemNoFieldsFilledInTest() {
+
+    }
+
+    public void AddItemToInventoryTest() {
+
+    }
+
+
+}
