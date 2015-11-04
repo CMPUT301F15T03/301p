b7fe7c4f5c2004bc6b918feedfb8ccb88f2576c2
Ross Anderson <ross.anderson@ualberta.ca>
2015-11-04 01:17:53 -0700

    Replace nav drawer activities with blank activities: ComposeTrade, ReviewTrade

--- T03/app/src/main/java/ca/ualberta/cmput301/t03/trading/TradeOfferReviewActivity.java
+++ T03/app/src/main/java/ca/ualberta/cmput301/t03/trading/TradeOfferReviewActivity.java
@@ -35,8 +35,7 @@ import android.view.View;
 
 import ca.ualberta.cmput301.t03.R;
 
-public class TradeOfferReviewActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class TradeOfferReviewActivity extends AppCompatActivity {
 
     private Trade model;
     private TradeOfferReviewController controller;
@@ -47,34 +46,6 @@ public class TradeOfferReviewActivity extends AppCompatActivity
         setContentView(R.layout.activity_trade_offer_review);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
     }
 
     @Override
@@ -97,30 +68,5 @@ public class TradeOfferReviewActivity extends AppCompatActivity
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camara) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
