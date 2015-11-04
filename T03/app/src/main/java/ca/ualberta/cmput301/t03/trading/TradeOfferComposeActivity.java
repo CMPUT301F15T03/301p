fc1c5f2750ab8e5fc42aae9ca8182f274fb6e9f7
Ross Anderson <ross.anderson@ualberta.ca>
2015-11-04 01:17:53 -0700

    Replace nav drawer activities with blank activities: ComposeTrade, ReviewTrade

--- T03/app/src/main/java/ca/ualberta/cmput301/t03/trading/TradeOfferComposeActivity.java
+++ T03/app/src/main/java/ca/ualberta/cmput301/t03/trading/TradeOfferComposeActivity.java
@@ -41,8 +41,7 @@ import ca.ualberta.cmput301.t03.R;
 import ca.ualberta.cmput301.t03.inventory.Item;
 import ca.ualberta.cmput301.t03.user.User;
 
-public class TradeOfferComposeActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+public class TradeOfferComposeActivity extends AppCompatActivity {
 
     private Trade model;
     private TradeOfferComposeController controller;
@@ -54,24 +53,6 @@ public class TradeOfferComposeActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
         model = new Trade(Parcels.<User>unwrap(getIntent().getParcelableExtra("trade/compose/borrower")),
                 Parcels.<User>unwrap(getIntent().getParcelableExtra("trade/compose/user")),
                 new ArrayList<Item>(),
@@ -80,16 +61,6 @@ public class TradeOfferComposeActivity extends AppCompatActivity
                 }},
                 this.getBaseContext());
         controller = new TradeOfferComposeController(this.getBaseContext(), model);
-    }
-
-    @Override
-    public void onBackPressed() {
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
     }
 
     @Override
@@ -112,30 +83,5 @@ public class TradeOfferComposeActivity extends AppCompatActivity
         }
 
         return super.onOptionsItemSelected(item);
-    }
-
-    @SuppressWarnings("StatementWithEmptyBody")
-    @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
-        int id = item.getItemId();
-
-        if (id == R.id.nav_camara) {
-            // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
-
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
-        }
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
     }
 }
