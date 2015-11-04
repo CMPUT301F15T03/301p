7b5f7a7d0139b99692e5b7473f5ec0bfd42a82ad
mabuyo <mabuyo@ualberta.ca>
2015-11-03 22:23:53 -0700

    Categories have a spinner now. Got photography categories from wikipedia.

--- T03/app/src/main/java/ca/ualberta/cmput301/t03/inventory/AddItemController.java
+++ T03/app/src/main/java/ca/ualberta/cmput301/t03/inventory/AddItemController.java
@@ -5,8 +5,10 @@ import android.content.Context;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -22,13 +24,33 @@ public class AddItemController {
     private View v;
     private Activity activity;
     private Inventory inventory;
+    Spinner categorySelect;
 
     public AddItemController(View v, Activity activity, Inventory inventory) {
         this.v = v;
         this.activity = activity;
         this.inventory = inventory;
         itemModel = new Item();
+
+        // listeners
+        categorySelect = (Spinner) v.findViewById(R.id.itemCategory);
+        categorySelect.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String category = (String) parent.getItemAtPosition(position);
+                itemModel.setItemCategory(category);
+
+                Log.d("ITEM", itemModel.getItemCategory());
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+//                Toast.makeText(v, "Please fill in the required fields.", Toast.LENGTH_LONG).show();
+//                return;
+            }
+        });
     }
+
 
     public void addItemToInventory() {
         // get fields
@@ -42,9 +64,6 @@ public class AddItemController {
         }
         EditText itemQualityText = (EditText) v.findViewById(R.id.itemQuality);
         String itemQuality = itemQualityText.getText().toString();
-
-        EditText itemCategoryText = (EditText) v.findViewById(R.id.itemCategory);
-        String itemCategory = itemCategoryText.getText().toString();
 
         CheckBox itemIsPrivateCheckBox = (CheckBox) v.findViewById(R.id.itemPrivateCheckBox);
         boolean itemIsPrivate = itemIsPrivateCheckBox.isChecked();
@@ -61,7 +80,6 @@ public class AddItemController {
         itemModel.setItemName(itemName);
         itemModel.setItemQuantity(itemQuantity);
         itemModel.setItemQuality(itemQuality);
-        itemModel.setItemCategory(itemCategory);
         itemModel.setItemIsPrivate(itemIsPrivate);
         itemModel.setItemDescription(itemDescription);
 
@@ -81,18 +99,15 @@ public class AddItemController {
     public boolean checkFieldsValid() {
         EditText itemNameText = (EditText) v.findViewById(R.id.itemName);
         String itemName = itemNameText.getText().toString();
-        EditText itemCategoryText = (EditText) v.findViewById(R.id.itemCategory);
-        String itemCategory = itemCategoryText.getText().toString();
 
         // the only things required are item name and category
         if (itemName.length() == 0) {
             return false;
-        } if (itemCategory.length() == 0) {
-            // TODO: Implement categories (i.e. drop down choices)
-            return false;
         }
+
         return true;
     }
+
 
     public void addItemToInventoryButtonClicked() {
         addItemToInventory();
