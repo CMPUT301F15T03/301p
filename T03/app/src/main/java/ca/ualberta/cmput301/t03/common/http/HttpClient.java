cf8aa9ec5253616b644dee6c2c6f22e701f0a1c8
Udey Rishi <udey.rishi@gmail.com>
2015-11-04 23:03:13 -0700

    Added javadocs for http module

--- T03/app/src/main/java/ca/ualberta/cmput301/t03/common/http/HttpClient.java
+++ T03/app/src/main/java/ca/ualberta/cmput301/t03/common/http/HttpClient.java
@@ -27,33 +27,70 @@ import java.net.MalformedURLException;
 import java.net.URL;
 
 /**
+ * A simple HTTP client for performing requests with major HTTP verbs.
  * Created by rishi on 15-10-31.
  */
 // Source: http://www.mkyong.com/java/how-to-send-http-request-getpost-in-java/
+// Date: 31-Oct-2015
 public class HttpClient {
 
     private URL root;
 
+    /**
+     * Creates an instance of {@link HttpClient}.
+     * @param root The root URL to be used for the requests.
+     * @throws MalformedURLException Thrown, if the root URL is malformed.
+     */
     public HttpClient(String root) throws MalformedURLException {
         this(new URL(root));
     }
 
+    /**
+     * Creates an instance of {@link HttpClient}/
+     * @param root The root {@link URL} for the requests.
+     */
     public HttpClient(URL root) {
         this.root = root;
     }
 
+    /**
+     * Performs an HTTP GET request at the URL formed using the provided suffix appended to the root.
+     * @param suffix The suffix to be appended to the root URL.
+     * @return The received {@link HttpResponse}.
+     * @throws IOException Thrown, if the network connection fails.
+     */
     public HttpResponse makeGetRequest(String suffix) throws IOException {
         return makeDataLessRequest(suffix, HttpMethods.GET);
     }
 
+    /**
+     * Performs an HTTP DELETE request at the URL formed using the provided suffix appended to the root.
+     * @param suffix The suffix to be appended to the root URL.
+     * @return The received {@link HttpResponse}.
+     * @throws IOException Thrown, if the network connection fails.
+     */
     public HttpResponse makeDeleteRequest(String suffix) throws IOException {
         return makeDataLessRequest(suffix, HttpMethods.DELETE);
     }
 
+    /**
+     * Performs an HTTP POST request at the URL formed using the provided suffix appended to the root.
+     * @param suffix The suffix to be appended to the root URL.
+     * @param postData The byte array to be sent as the contents of the request.
+     * @return The received {@link HttpResponse}.
+     * @throws IOException Thrown, if the network connection fails.
+     */
     public HttpResponse makePostRequest(String suffix, byte[] postData) throws IOException {
         return makeSendDataRequest(suffix, postData, HttpMethods.POST);
     }
 
+    /**
+     * Performs an HTTP PUT request at the URL formed using the provided suffix appended to the root.
+     * @param suffix The suffix to be appended to the root URL.
+     * @param putData The byte array to be sent as the contents of the request.
+     * @return The received {@link HttpResponse}.
+     * @throws IOException Thrown, if the network connection fails.
+     */
     public HttpResponse makePutRequest(String suffix, byte[] putData) throws IOException {
         return makeSendDataRequest(suffix, putData, HttpMethods.PUT);
     }
