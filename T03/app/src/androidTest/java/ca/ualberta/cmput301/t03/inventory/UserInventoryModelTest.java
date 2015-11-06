c92a17e05ae427d9573bf60865c1b51a29eb52ca
Quentin Lautischer <lautisch@ualberta.ca>
2015-11-06 15:08:45 -0700

    Added model tests

---
+++ T03/app/src/androidTest/java/ca/ualberta/cmput301/t03/inventory/UserInventoryModelTest.java
@@ -0,0 +1,33 @@
+package ca.ualberta.cmput301.t03.inventory;
+
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import ca.ualberta.cmput301.t03.MainActivity;
+import ca.ualberta.cmput301.t03.commontesting.PrimaryUserHelper;
+
+import static junit.framework.Assert.assertEquals;
+
+/**
+ * Created by quentinlautischer on 2015-11-06.
+ */
+@RunWith(AndroidJUnit4.class)
+@LargeTest
+public class UserInventoryModelTest {
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class);
+
+    @Test
+    public void testInventoryInit(){
+        Inventory inventory = new Inventory();
+        assertEquals(inventory.getItems().size(), 0);
+    }
+
+
+}
