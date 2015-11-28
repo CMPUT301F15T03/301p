002df56e4b7c8e9df882e34728768a2af6f70b5b
Udey Rishi <udey@ualberta.ca>
2015-11-28 01:09:43 -0700

    Improved the 'testFormQuery' test

--- T03/app/src/androidTest/java/ca/ualberta/cmput301/t03/datamanager/queries/FieldGroupedQueryTests.java
+++ T03/app/src/androidTest/java/ca/ualberta/cmput301/t03/datamanager/queries/FieldGroupedQueryTests.java
@@ -1,9 +1,10 @@
-package ca.ualberta.cmput301.t03.datamanager;
+package ca.ualberta.cmput301.t03.datamanager.queries;
 
 import junit.framework.TestCase;
 
 import java.util.ArrayList;
 
+import ca.ualberta.cmput301.t03.datamanager.JsonFormatter;
 import ca.ualberta.cmput301.t03.datamanager.elasticsearch.queries.FieldGroupedQuery;
 import ca.ualberta.cmput301.t03.trading.TradeStateAccepted;
 import ca.ualberta.cmput301.t03.trading.TradeStateOffered;
@@ -12,6 +13,23 @@ import ca.ualberta.cmput301.t03.trading.TradeStateOffered;
  * Created by rishi on 15-11-27.
  */
 public class FieldGroupedQueryTests extends TestCase {
+
+    private static final String expectedQueryJson = "{\n" +
+            "  \"aggs\": {\n" +
+            "    \"group_by_owner_username\": {\n" +
+            "      \"terms\": {\n" +
+            "        \"field\": \"owner.username\",\n" +
+            "        \"size\": 5\n" +
+            "      }\n" +
+            "    }\n" +
+            "  },\n" +
+            "  \"query\": {\n" +
+            "    \"query_string\": {\n" +
+            "      \"default_field\": \"state\",\n" +
+            "      \"query\": \"TradeStateOffered OR TradeStateAccepted\"\n" +
+            "    }\n" +
+            "  }\n" +
+            "}";
 
     public void testFormQuery() {
         FieldGroupedQuery query = new FieldGroupedQuery("state", new ArrayList<String>() {
@@ -22,6 +40,7 @@ public class FieldGroupedQueryTests extends TestCase {
         }, "owner.username", 5);
 
         String queryJson = query.formQuery();
+        assertEquals(expectedQueryJson, queryJson);
 
         JsonFormatter formatter = new JsonFormatter(false, true);
         FieldGroupedQuery query2 = formatter.getGson().fromJson(queryJson, FieldGroupedQuery.class);
