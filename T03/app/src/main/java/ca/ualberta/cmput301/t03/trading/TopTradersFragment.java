0b1aab3145a3a0050d2e4699e1e4592834206970
Udey Rishi <udey.rishi@gmail.com>
2015-11-28 15:54:57 -0700

    Implemented TopTradersFragment
    - Integrated with MainActivity for loading from the navigation pane
    - Loads the data and populates the TableLayout
    - Needs visual polish

---
+++ T03/app/src/main/java/ca/ualberta/cmput301/t03/trading/TopTradersFragment.java
@@ -0,0 +1,92 @@
+package ca.ualberta.cmput301.t03.trading;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.support.v4.app.Fragment;
+import android.widget.TableRow;
+import android.widget.TextView;
+
+import java.io.IOException;
+import java.util.HashMap;
+
+import ca.ualberta.cmput301.t03.R;
+import ca.ualberta.cmput301.t03.TradeApp;
+
+/**
+ * Source (Icon): https://developers.google.com/games/services/images/branding/ic_play_games_badge_leaderboards_gray.png
+ * Accessed: 28 Nov
+ * Created by rishi on 15-11-28.
+ */
+public class TopTradersFragment extends Fragment {
+    private static final int TOP_TRADERS_COUNT = 10;
+
+    private TopTradersProvider refreshController;
+    private TableLayout topTradersLayout;
+
+    private HashMap<String, Integer> topTraders;
+
+    public TopTradersFragment() {
+
+    }
+
+    public static TopTradersFragment newInstance() {
+        return new TopTradersFragment();
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        refreshController = new TopTradersProvider(TradeApp.getInstance().createQueryExecutor());
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_top_traders, container, false);
+        topTradersLayout = (TableLayout) rootView.findViewById(R.id.top_traders_grid);
+        onRefresh();
+        return rootView;
+    }
+
+    public void onRefresh() {
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    topTraders = refreshController.getTopTraders(TOP_TRADERS_COUNT);
+                    refreshTopTradersUI();
+                } catch (IOException e) {
+                    // TODO: Handle this
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        thread.start();
+    }
+
+    private void refreshTopTradersUI() {
+        getActivity().runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                topTradersLayout.removeAllViews();
+
+                for (String userName : topTraders.keySet()) {
+                    Integer successfulTrades = topTraders.get(userName);
+
+                    TableRow row = (TableRow)LayoutInflater.from(TopTradersFragment.this.getContext())
+                            .inflate(R.layout.attrib_row, null);
+                    ((TextView)row.findViewById(R.id.attrib_name)).setText(userName);
+                    ((TextView)row.findViewById(R.id.attrib_value)).setText(successfulTrades.toString());
+                    topTradersLayout.addView(row);
+                }
+            }
+        });
+    }
+
+
+}
