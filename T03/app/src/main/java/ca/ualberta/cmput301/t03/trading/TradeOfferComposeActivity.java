6f09cfc8487943eb2654cf25844a8d871a9d9793
Ross Anderson <ross.anderson@ualberta.ca>
2015-11-27 18:26:11 -0700

    TradeCompose: fetch & display owner and borrower items using listviews and EnhancedSimpleAdapter

--- T03/app/src/main/java/ca/ualberta/cmput301/t03/trading/TradeOfferComposeActivity.java
+++ T03/app/src/main/java/ca/ualberta/cmput301/t03/trading/TradeOfferComposeActivity.java
@@ -22,23 +22,31 @@ package ca.ualberta.cmput301.t03.trading;
 
 import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.parceler.Parcels;
 
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.UUID;
 
 import ca.ualberta.cmput301.t03.R;
 import ca.ualberta.cmput301.t03.common.exceptions.ServiceNotAvailableException;
+import ca.ualberta.cmput301.t03.inventory.EnhancedSimpleAdapter;
 import ca.ualberta.cmput301.t03.inventory.Item;
 import ca.ualberta.cmput301.t03.user.User;
 
@@ -58,6 +66,17 @@ public class TradeOfferComposeActivity extends AppCompatActivity {
     private Button cancelButton;
     private Button addItemButton;
 
+    private ListView ownerItemListView;
+    private EnhancedSimpleAdapter ownerItemAdapter;
+    private List<HashMap<String, Object>> ownerItemTiles;
+    private HashMap<Integer, UUID> ownerItemTilePositionMap;
+
+    private ListView borrowerItemListView;
+    private EnhancedSimpleAdapter borrowerItemAdapter;
+    private List<HashMap<String, Object>> borrowerItemTiles;
+    private HashMap<Integer, UUID> borrowerItemTilePositionMap;
+
+
     /**
      * Sets up the view with all components, the model, and the controller
      *
@@ -74,6 +93,12 @@ public class TradeOfferComposeActivity extends AppCompatActivity {
         offerButton = (Button) findViewById(R.id.tradeComposeOffer);
         cancelButton = (Button) findViewById(R.id.tradeComposeCancel);
         addItemButton = (Button) findViewById(R.id.tradeComposeAddItem);
+
+        ownerItemListView = (ListView) findViewById(R.id.tradeComposeOwnerItem);
+        borrowerItemListView = (ListView) findViewById(R.id.tradeComposeBorrowerItems);
+
+        ownerItemTilePositionMap = new HashMap<>();
+        borrowerItemTilePositionMap = new HashMap<>();
 
         AsyncTask worker = new AsyncTask() {
             @Override
@@ -95,15 +120,29 @@ public class TradeOfferComposeActivity extends AppCompatActivity {
                     throw new RuntimeException("App is offline.", e);
                 }
                 controller = new TradeOfferComposeController(c, model);
-                return null;
+
+                ownerItemTiles = buildItemTiles(model.getOwnersItems(), ownerItemTilePositionMap);
+                borrowerItemTiles = buildItemTiles(model.getBorrowersItems(), borrowerItemTilePositionMap);
+
+                return c;
             }
 
             @Override
             protected void onPostExecute(Object o) {
                 super.onPostExecute(o);
+                final Context c = (Context) o;
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
+                        String[] from = {"tileViewItemName", "tileViewItemCategory", "tileViewItemImage"};
+                        int[] to = {R.id.tileViewItemName, R.id.tileViewItemCategory, R.id.tileViewItemImage};
+
+                        ownerItemAdapter = new EnhancedSimpleAdapter(c, ownerItemTiles, R.layout.fragment_item_tile, from, to);
+                        ownerItemListView.setAdapter(ownerItemAdapter);
+
+                        borrowerItemAdapter = new EnhancedSimpleAdapter(c, borrowerItemTiles, R.layout.fragment_item_tile, from, to);
+                        borrowerItemListView.setAdapter(borrowerItemAdapter);
+
                         ownerUsername.setText(model.getOwner().getUsername());
                         offerButton.setOnClickListener(new View.OnClickListener() {
                             @Override
@@ -138,6 +177,28 @@ public class TradeOfferComposeActivity extends AppCompatActivity {
             }
         };
         worker.execute(getBaseContext());
+    }
+
+    private ArrayList<HashMap<String, Object>> buildItemTiles(List<Item> items, HashMap<Integer, UUID> positionMap) {
+        ArrayList<HashMap<String, Object>> tiles = new ArrayList<>();
+
+        int i = 0;
+        positionMap.clear();
+        for (Item item : items) {
+            HashMap<String, Object> hm = new HashMap<String, Object>();
+            hm.put("tileViewItemName", item.getItemName());
+            hm.put("tileViewItemCategory", item.getItemCategory());
+            if(item.getPhotoList().getPhotos().size() > 0){
+                hm.put("tileViewItemImage", (Bitmap) item.getPhotoList().getPhotos().get(0).getPhoto());
+            } else {
+                hm.put("tileViewItemImage", ((BitmapDrawable) getResources().getDrawable(R.drawable.photo_unavailable)).getBitmap());
+            }
+            tiles.add(hm);
+            positionMap.put(i, item.getUuid());
+            i++;
+        }
+
+        return tiles;
     }
 
     /**
