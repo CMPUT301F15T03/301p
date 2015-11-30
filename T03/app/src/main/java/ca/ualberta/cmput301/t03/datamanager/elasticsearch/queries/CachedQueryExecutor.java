c3e8d38002f2bb54c310555eea977084fca7e03d
Udey Rishi <udey.rishi@gmail.com>
2015-11-29 19:03:22 -0700

    Added javadocs for queries

--- T03/app/src/main/java/ca/ualberta/cmput301/t03/datamanager/elasticsearch/queries/CachedQueryExecutor.java
+++ T03/app/src/main/java/ca/ualberta/cmput301/t03/datamanager/elasticsearch/queries/CachedQueryExecutor.java
@@ -9,6 +9,9 @@ import ca.ualberta.cmput301.t03.datamanager.DataKey;
 import ca.ualberta.cmput301.t03.datamanager.LocalDataManager;
 
 /**
+ * A {@link QueryExecutor} that uses an inner {@link QueryExecutor} to obtain results, and caches the
+ * results. For subsequent requests, if an {@link IOException} is thrown, the cache is used to obtain
+ * the results.
  * Created by rishi on 15-11-28.
  */
 public class CachedQueryExecutor implements QueryExecutor {
@@ -16,11 +19,24 @@ public class CachedQueryExecutor implements QueryExecutor {
     private final QueryExecutor innerExecutor;
     private final LocalDataManager cachingDataManager;
 
+    /**
+     * Creates an instance of {@link CachedQueryExecutor}.
+     * @param innerExecutor The inner {@link QueryExecutor} to be used for executing the first chance
+     *                      queries.
+     */
     public CachedQueryExecutor(QueryExecutor innerExecutor) {
         this.innerExecutor = Preconditions.checkNotNull(innerExecutor, "innerExecutor");
         this.cachingDataManager = new LocalDataManager();
     }
 
+    /**
+     * Performs the query and gets the {@link AggregationQueryResult} obtained back. For subsequent
+     * requests, if an {@link IOException} is thrown, the cache is used to obtain the results.
+     * @param suffix The suffix at which the query should be executed.
+     * @param query The {@link Query} to be executed.
+     * @return The obtained {@link AggregationQueryResult}.
+     * @throws IOException Thrown, if there is some network failure.
+     */
     @Override
     public AggregationQueryResult executeQuery(String suffix, Query query) throws IOException {
         AggregationQueryResult aggregationQueryResult;
--- T03/app/src/main/java/ca/ualberta/cmput301/t03/datamanager/elasticsearch/queries/HttpQueryExecutor.java
+++ T03/app/src/main/java/ca/ualberta/cmput301/t03/datamanager/elasticsearch/queries/HttpQueryExecutor.java
@@ -7,12 +7,16 @@ import ca.ualberta.cmput301.t03.datamanager.JsonFormatter;
 import ca.ualberta.cmput301.t03.datamanager.elasticsearch.ElasticSearchHelper;
 
 /**
+ * A {@link QueryExecutor} that performs HTTP requests to Elastic Search to get the results.
  * Created by rishi on 15-11-28.
  */
 public class HttpQueryExecutor implements QueryExecutor {
 
     private final ElasticSearchHelper elasticSearchHelper = new ElasticSearchHelper();
 
+    /**
+     * {@inheritDoc}
+     */
     @Override
     public AggregationQueryResult executeQuery(String suffix, Query query) throws IOException {
         Preconditions.checkNotNull(query, "query");
