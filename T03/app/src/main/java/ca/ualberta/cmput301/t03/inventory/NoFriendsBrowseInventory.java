6673bdb0ffeb2b4d80cda110bb9b09a47af57909
Udey Rishi <udey.rishi@gmail.com>
2015-11-30 01:46:17 -0700

    Added a intro page for Browse Inventories when you have no friends
    - This is probably the wrong way of doing MVC, but I'm keeping it the same way as BrowseInventoryFragment
    - There is some issue with no_friends_layout.xml. The pull down refresh circle kinda stutters

---
+++ T03/app/src/main/java/ca/ualberta/cmput301/t03/inventory/NoFriendsBrowseInventory.java
@@ -0,0 +1,127 @@
+package ca.ualberta.cmput301.t03.inventory;
+
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import ca.ualberta.cmput301.t03.Observable;
+import ca.ualberta.cmput301.t03.Observer;
+import ca.ualberta.cmput301.t03.PrimaryUser;
+import ca.ualberta.cmput301.t03.R;
+import ca.ualberta.cmput301.t03.common.exceptions.ServiceNotAvailableException;
+import ca.ualberta.cmput301.t03.filters.FilterCriteria;
+
+/**
+ * Created by rishi on 15-11-30.
+ */
+public class NoFriendsBrowseInventory extends Fragment implements Observer, SwipeRefreshLayout.OnRefreshListener {
+
+    private View mView;
+    private Inventory model;
+    private SwipeRefreshLayout mSwipeRefreshLayout;
+    private Object uiLock = new Object();
+
+    public NoFriendsBrowseInventory() {
+        // Required empty public constructor
+    }
+
+    public static NoFriendsBrowseInventory newInstance() {
+        return new NoFriendsBrowseInventory();
+    }
+
+    /**
+     * Gets User associated to system config. Loads any data associated to Browsables.
+     * Namely Users friendList.
+     *
+     * @param savedInstanceState
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        onRefresh();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        synchronized (uiLock) {
+            mView = inflater.inflate(R.layout.no_friends_layout, container, false);
+            mSwipeRefreshLayout = (SwipeRefreshLayout) mView.findViewById(R.id.noFriendsSwipeLayout);
+            mSwipeRefreshLayout.setOnRefreshListener(this);
+        }
+        return mView;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void onDestroy() {
+        if (model != null){
+            model.removeObserver(this);
+        }
+        super.onDestroy();
+    }
+
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.inventory_filter_menu_items, menu);
+    }
+
+    @Override
+    public void update(Observable observable) {
+        onRefresh();
+    }
+
+    @Override
+    public void onRefresh() {
+        AsyncTask<Void, Void, Void> task = new AsyncTask<Void, Void, Void>() {
+            @Override
+            protected Void doInBackground(Void... params) {
+                try {
+                    PrimaryUser.getInstance().refresh();
+                    model = PrimaryUser.getInstance().getBrowseableInventories(new ArrayList<FilterCriteria>());
+                    //fixme browseableinventories actually needs to refresh
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                } catch (ServiceNotAvailableException e) {
+                    throw new RuntimeException(e);
+                }
+                return null;
+            }
+
+            @Override
+            protected void onPostExecute(Void aVoid) {
+                if (mSwipeRefreshLayout.isRefreshing()) {
+                    mSwipeRefreshLayout.setRefreshing(false);
+                }
+
+                if (model.size() != 0) {
+                    synchronized (uiLock) {
+                        getFragmentManager().beginTransaction()
+                                .replace(R.id.fragmentContent, BrowseInventoryFragment.newInstance())
+                                .commit();
+                    }
+                }
+            }
+        };
+        task.execute();
+    }
+}
