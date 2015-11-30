b50f32e9f0a78a4965ba24f94cf8bca184d68d8b
Quentin Lautischer <lautisch@ualberta.ca>
2015-11-30 03:53:45 -0700

    updated inventory tests

--- T03/app/src/androidTest/java/ca/ualberta/cmput301/t03/inventory/BrowseFriendInventoryUITest.java
+++ T03/app/src/androidTest/java/ca/ualberta/cmput301/t03/inventory/BrowseFriendInventoryUITest.java
@@ -1,11 +1,22 @@
 package ca.ualberta.cmput301.t03.inventory;
 
+import android.content.Intent;
+import android.support.test.InstrumentationRegistry;
 import android.support.test.espresso.NoMatchingViewException;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
 
 import org.hamcrest.core.StringContains;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import ca.ualberta.cmput301.t03.MainActivity;
 import ca.ualberta.cmput301.t03.R;
+import ca.ualberta.cmput301.t03.common.PrimaryUserHelper;
 
 import static android.support.test.espresso.Espresso.onData;
 import static android.support.test.espresso.Espresso.onView;
@@ -25,18 +36,27 @@ import static org.hamcrest.Matchers.hasToString;
 /**
  * Created by quentinlautischer on 2015-11-29.
  */
+
+@RunWith(AndroidJUnit4.class)
+@LargeTest
 public class BrowseFriendInventoryUITest {
 
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule = new ActivityTestRule<>(
+            MainActivity.class, false, false);
 
-    /**
-     * UC3.1.2 BrowseFriendGeneralSearch
-     * Precondition User exists with friends that have inventories,
-     * with both private and public items.
-     */
-    @Test
-    public void testBrowseFriendFromFriends() throws Exception {
+    private MainActivity mActivity = null;
+
+    @Before
+    public void setActivity() {
+        PrimaryUserHelper.createAndLoadUserWithFriendThatHasInventory(InstrumentationRegistry.getTargetContext());
+
+        mActivity = mActivityRule.launchActivity(new Intent());
+        InstrumentationRegistry.getInstrumentation();
+        pause();
+
         /**
-         * Navigate to FriendList and Find Friend
+         * Go to inventory friends inventory
          */
         onView(withContentDescription("Open navigation drawer")).check(matches(isDisplayed())).perform(click());
         pause();
@@ -53,6 +73,24 @@ public class BrowseFriendInventoryUITest {
          */
         onView(withText("Inventory")).check(matches(isDisplayed())).perform(click());
         pause();
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        PrimaryUserHelper.deleteAndUnloadUserWithFriendThatHasInventory(InstrumentationRegistry.getTargetContext());
+    }
+
+    /**
+     * UC3.1.2 BrowseFriendGeneralSearch
+     * Precondition User exists with friends that have inventories,
+     * with both private and public items.
+     */
+    @Test
+    public void testBrowseFriendFromFriends() throws Exception {
+        /**
+         * Navigate to FriendList and Find Friend
+         */
+
 
         /**
          * Checks that there are two items from the filtered friend
@@ -81,7 +119,38 @@ public class BrowseFriendInventoryUITest {
          * Adding filter of a textualQuery and friend to view one friends inventory,
          * filtered by a given textualQuery.
          */
+        onView(withContentDescription("Search"))
+                .check(matches(isDisplayed()))
+                .perform(click());
 
+        onView(withId(R.id.addSearchFilterText)).
+                perform(typeText("item"),
+                        closeSoftKeyboard());
+        pause();
+        onView(withText("Add")).
+                perform(click());
+        pause();
+
+        /**
+         * Checks that there are two items from the filtered friends and that have the
+         * textual string "FX10"
+         */
+        onView(withText("testItem1f1")).check(matches(isDisplayed()));
+        onView(withText("testItem2f1")).check(matches(isDisplayed()));
+
+        onView(withContentDescription("Search"))
+                .check(matches(isDisplayed()))
+                .perform(click());
+
+        onView(withId(R.id.addSearchFilterText)).
+                perform(typeText("2"),
+                        closeSoftKeyboard());
+        pause();
+        onView(withText("Add")).
+                perform(click());
+        pause();
+
+        onView(withText("testItem2f1")).check(matches(isDisplayed()));
     }
 
     /**
